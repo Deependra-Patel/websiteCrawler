dad516a2ed19f1cde3407dc735525fdca0ce9195
Deependra Patel <pateldeependra06@gmail.com>
2019-04-07 15:10:43 +0100

    Extracted all links out of the page

--- hello.go
+++ hello.go
@@ -4,17 +4,35 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 func main() {
-	url := "https://github.com"
+	urls := GetExternalLinks("https://github.com")
+	fmt.Print(urls)
+}
+
+func GetExternalLinks(url string) []string {
+	log.Print("Getting ", url)
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Printf("Call failed for url: %s with error: %s", url, err)
+		log.Panicf("Call failed for url: %s with error: %s", url, err)
+		return nil
 	} else {
 		buffer := make([]byte, 1024*1024)
-		_, _ = resp.Body.Read(buffer)
-		fmt.Print(string(buffer))
+		count, err := resp.Body.Read(buffer)
+		if err != nil {
+			log.Panicf("Reading response body for url %s failed with %s", url, err)
+		}
+		log.Printf("Number of bytes read %d", count)
+		return GetUrls(string(buffer))
 	}
-	fmt.Print("Test")
+}
+
+func GetUrls(body string) []string {
+	compile, err := regexp.Compile("href=\"[^\"]*\"")
+	if err != nil {
+		panic(err)
+	}
+	return compile.FindAllString(body, -1)
 }
