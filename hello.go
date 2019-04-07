ce0381b842c1f1be3b3eeb3f6576a20455afa44f
Deependra Patel <pateldeependra06@gmail.com>
2019-04-07 15:46:12 +0100

    Parsing url to handle relative links on the webpage

--- hello.go
+++ hello.go
@@ -4,35 +4,45 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 )
 
 func main() {
-	urls := GetExternalLinks("https://github.com")
+	link, _ := url.Parse("https://github.com")
+	urls := GetExternalLinks(link)
 	fmt.Print(urls)
 }
 
-func GetExternalLinks(url string) []string {
-	log.Print("Getting ", url)
-	resp, err := http.Get(url)
+func GetExternalLinks(link *url.URL) []*url.URL {
+	log.Print("Getting ", link)
+	resp, err := http.Get(link.String())
 	if err != nil {
-		log.Panicf("Call failed for url: %s with error: %s", url, err)
+		log.Panicf("Call failed for link: %s with error: %s", link, err)
 		return nil
 	} else {
 		buffer := make([]byte, 1024*1024)
 		count, err := resp.Body.Read(buffer)
 		if err != nil {
-			log.Panicf("Reading response body for url %s failed with %s", url, err)
+			log.Panicf("Reading response body for link %s failed with %s", link, err)
 		}
 		log.Printf("Number of bytes read %d", count)
-		return GetUrls(string(buffer))
+		return GetUrls(link, string(buffer))
 	}
 }
 
-func GetUrls(body string) []string {
+func GetUrls(parse *url.URL, body string) []*url.URL {
 	compile, err := regexp.Compile("href=\"[^\"]*\"")
 	if err != nil {
 		panic(err)
 	}
-	return compile.FindAllString(body, -1)
+	links := make([]*url.URL, 0)
+	for _, str := range compile.FindAllString(body, -1) {
+		link, err := parse.Parse(str[6 : len(str)-1])
+		if err != nil {
+			panic(err)
+		}
+		links = append(links, link)
+	}
+	return links
 }
